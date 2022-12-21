c4606017639781cd2adeba001386ad52962b3e9e
mhamdNaser <115448369+mhamdNaser@users.noreply.github.com>
2022-12-21 11:20:33 +0300

    create config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+    $hostName = "localhost";
+    $dbname = "crud";
+    $dbuser = "root";
+    $dbPassword = "";
+    
+    $connect_db = mysqli_connect($hostName,$dbuser,$dbPassword,$dbname);
+
+
+    // check connection 
+    if(!$connect_db){
+        echo("CONNECTION ERROR" . mysqli_connect_error());
+    }
+    else{
+        echo("DataBase IS Connected")
+    }
